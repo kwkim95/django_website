75073e19738103e9163c532092c43a7be70d43e4
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-07-22 19:39:03 +0900

    create Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# post
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author
+
+    def __str__(self):
+        return '[{0}] {1}'.format(self.pk, self.title)
+
