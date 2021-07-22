c4ded0cf8ef3c47a7befce78c7d336ffc81ed08c
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-07-22 20:24:33 +0900

    make blog post list by FBV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.order_by('-created_at')
+    context = {'posts':posts}
+    return render(request, 'blog/index.html', context)
+
