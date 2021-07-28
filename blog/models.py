136025dc7b6ec2a7c51967327f5ce92e21782a01
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-07-28 21:54:43 +0900

    make category page

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User
 import os
 
 
-# # categpry
+# categpry
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return '/blog/category/{0}'.format(self.slug)
 
     class Meta:
         verbose_name_plural = 'Categories'
