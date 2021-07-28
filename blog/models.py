1f90bc0965c8c8e47abbc19a44769fac1a0d9908
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-07-28 20:56:07 +0900

    add category at post list page

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 import os
 
 
+# # categpry
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 # post
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -17,6 +29,8 @@ class Post(models.Model):
     # author
 
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return '[{0}] {1} :: {2}'.format(self.pk, self.title, self.author)
