ec17c1ae5903cb72602f792ed5809aba922c68a7
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-08-17 22:11:27 +0900

    add comment delete function

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,16 @@ from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
 
 
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+    
+
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
     form_class = CommentForm
