e7bdd5a1a5d9c92978b9b5b9ed733f1e814fb0c8
KeonWoo Kim <52156247+kwkim95@users.noreply.github.com>
2021-09-16 22:58:27 +0900

    put django at docker

--- website/settings.py
+++ website/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&_e8jlow03iekem^^z)l_o12iw5&))h=rd$=ym)9l$$v0o!)^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--&_e8jlow03iekem^^z)l_o12iw5&))h=rd$=ym)9l$$v0o!)^')
+# SECRET_KEY = 'django-insecure--&_e8jlow03iekem^^z)l_o12iw5&))h=rd$=ym)9l$$v0o!)^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
